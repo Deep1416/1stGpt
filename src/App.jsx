46c430dcfe19b1e1b48c695224755b6e0f1ec932
Shubham Chopde <shubhamchopde090@gmail.com>
2024-07-30 16:25:50 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from './pages/HomePage';
-import GetStarted from './pages/GetStarted';
 import Pricing from './getStarted/pricing/Pricing';
 import Layout from './getStarted/layout/Layout';
-// import HomePage from './getStarted/home/HomePage'
 import Aimodals from './getStarted/aiModals/Aimodals'
 import ChatBot from './getStarted/chat/ChatBot'
+
 const App = () => {
   return (
     <div>
@@ -16,8 +15,9 @@ const App = () => {
         <Route path="/get" element={<Layout />}>
           <Route index element={<ChatBot />} />
           <Route path="aimodels" element={<Aimodals />} />
+          <Route path="profile" element={<Aimodals />} />
+          <Route path="pricing" element={<Pricing />} />
         </Route>
-  
       </Routes>
     </BrowserRouter>
     </div>
