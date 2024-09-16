465424ae9534abfcbc315b243807a300882b888d
Shubham Chopde <shubhamchopde090@gmail.com>
2024-09-16 15:44:37 +0530

    add redirect

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // import React from 'react'
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
 import HomePage from './pages/HomePage';
 import Pricing from './getStarted/pricing/Pricing';
 import Layout from './getStarted/layout/Layout';
@@ -9,19 +9,22 @@ import ChatBot from './getStarted/chat/ChatBot'
 import Profile from './getStarted/userProfile/Profile'
 
 const App = () => {
+  const navigate = useNavigate();
 
   useEffect(() => {
     // Check if token is expired on page load
     const token = localStorage.getItem('token');
     if (token) {
+      console.log(token)
       const payload = JSON.parse(atob(token.split('.')[1])); // Decoding JWT token
       if (payload.exp * 1000 < Date.now()) {
         // Token is expired
         localStorage.removeItem('token');
         localStorage.removeItem('userInfo');
+        navigate('/');
       }
     }
-  }, []);
+  } ,[]);
 
   return (
     <div>
